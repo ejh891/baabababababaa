0f16fd61116de419c91ca08a942fc2fd97c92d9e
EJ Hammond <ejh891@gmail.com>
2017-04-07 11:38:40 -0400

    Add simple server and existing app code

---
+++ public/scripts/default.js
@@ -0,0 +1,60 @@
+var $audio;
+var $count;
+var counter;
+
+document.addEventListener("DOMContentLoaded", function (ev) {
+    $audio = document.getElementById("audio");
+    $count = document.getElementById("count");
+    counter = 0;
+
+    $audio.load();
+    getServerSideCount();
+
+    bindAudioFunctions();
+});
+
+function bindAudioFunctions() {
+    $audio.onplay = function () {
+        $(".button").prop("disabled", true);
+        $("#logo").addClass("spin");
+    }
+
+    $audio.onended = function () {
+        $(".button").prop("disabled", false);
+        $("#logo").removeClass("spin");
+    }
+}
+
+function buttonClicked() {
+    play();
+    incrementCounter();
+}
+
+function play() {
+    $audio.play();
+}
+
+function displayCount() {
+    console.log(counter);
+    $count.innerHTML = counter;
+}
+
+function getServerSideCount() {
+    $.ajax({
+        url: "res/count.txt?"+ Math.random(),
+        method: "GET",
+        success: function(data) {
+            counter = data;
+            displayCount();
+        },
+        error: function(xhr, textstatus, error) {
+            console.error(xhr, textstatus, error);
+        }
+    });
+}
+
+function incrementCounter() {
+    $.ajax({ url: "scripts/incrementCounter.php" });
+    counter++;
+    displayCount();
+}
